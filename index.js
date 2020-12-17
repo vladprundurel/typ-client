0766c8bbda0c3e268f17f049d5d05c5c9ec20783
Vlad <vladprundurel1@gmail.com>
2020-12-17 03:01:07 +0200

    d n_m

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/dist/client'));
 
-app.get('/', (req, res)=>{
-    res.render('index.html');
+app.get('/*', function(req,res) {
+    
+    res.sendFile(path.join(__dirname+'/dist/client/index.html'));
 });
+// app.get('/', (req, res)=>{
+//     res.render('index.html');
+// });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
